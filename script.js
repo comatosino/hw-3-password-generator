187c3f80ed604b2cf4cbecb2e8b2e66a30744530
Robert Adams <adamsiii.robert@gmail.com>
2021-03-25 21:37:42 -0700

    reworded user prompts

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-// Assignment Code
-//DO NOT CHANGE THIS CODE
-var generateBtn = document.querySelector("#generate");
-
 // create arrays for each of lowercase, uppercase, numbers, and special characters
 var lowerAlphaArray = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperAlphaArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numsArray = "0123456789".split("");
 var specCharsArray = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
 
+// Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// generates the password
 // returns a string of given length of random characters of specified type
 // returns corresponding message if no length entered or char types selected
 function generatePassword () {
   var passwordLength = getPasswordLength();
+  console.log(passwordLength);
 
   // if no length entered, return message
   if (passwordLength == null) {
     return "Password not generated: No valid length entered";
+
   // else proceed to select character types
   } else {
     var passwordArray = getPasswordArray();
@@ -25,11 +35,12 @@ function generatePassword () {
       return "Password not generated: No character types selected"; 
     }
 
-    // length and char types given, generate the password and return it
+    // generate the password
     var extraSecretPassword = "";
     for (i = 0; i < passwordLength; i++) {
       var randomIndex = Math.floor(Math.random() * passwordArray.length)
       extraSecretPassword = extraSecretPassword.concat(passwordArray[randomIndex]);
+      console.log(i);
     }
     return extraSecretPassword;
   }  
@@ -43,7 +54,7 @@ function getPasswordLength() {
 
   // if user does not click cancel, keep prompting
   while (userInput != null) {
-    userInput = window.prompt("Please enter a password length of at least 8 characters and no more than 128 characters.", "");
+    userInput = window.prompt("Please enter a password length of at least 8 characters and no more than 128 characters. Select \"Cancel\" to quit.", "");
     numChars = Number(userInput);
     console.log(userInput);
     console.log(numChars);
@@ -62,44 +73,34 @@ function getPasswordLength() {
 // returns an array of selected character types
 function getPasswordArray () {
   var charTypesArray = [];
+  console.log(charTypesArray);
 
   // prompt user to include lowercase alphabet chars
-  var useLowerAlpha = window.confirm("use lowercase?");
+  var useLowerAlpha = window.confirm("Would you like to include lowercase letters?");
   if (useLowerAlpha) {
     charTypesArray = charTypesArray.concat(lowerAlphaArray);
   }
+  console.log(charTypesArray);
 
   // prompt user to include uppercase alphabet characters
-  var useUpperAlpha = window.confirm("use uppercase?");
+  var useUpperAlpha = window.confirm("Would you like to include uppercase letters?");
   if (useUpperAlpha) {
     charTypesArray = charTypesArray.concat(upperAlphaArray);
   }
+  console.log(charTypesArray);
 
   // prompt user to include numbers 0-9
-  var useNums = window.confirm("use numbers?");
+  var useNums = window.confirm("Would you like to include numbers?");
   if (useNums) {
     charTypesArray = charTypesArray.concat(numsArray);
   }
+  console.log(charTypesArray);
 
   // prompt user to include special characters
-  var useSpecChars = window.confirm("use special characters?");
+  var useSpecChars = window.confirm("Would you like to include special characters?");
   if (useSpecChars) {
     charTypesArray = charTypesArray.concat(specCharsArray);
   }
   console.log(charTypesArray);
   return charTypesArray;
 }
-
-// Write password to the #password input
-/* DO NOT CHANGE THIS CODE!  This function is going to call your generatePassword() function and whatever is returned from that function will be put onto the page */
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-// DO NOT CHANGE THIS CODE
-generateBtn.addEventListener("click", writePassword);
