15a3908028b83f8a8957a77c33206c8812190910
Robert Adams <adamsiii.robert@gmail.com>
2022-09-17 16:37:44 -0700

    Rename file

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 const RANGE = /\b([8-9]|[1-9][0-9]|1[01][0-9]|12[0-8])\b/;
+
 const CHARS = {
   uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
   lowercase: 'abcdefghijklmnopqrstuvwxyz'.split(''),
